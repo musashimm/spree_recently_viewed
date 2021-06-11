09f525a20ae190bb72688ebbe199198356abb107
Wojciech Todryk <wojciech@todryk.pl>
2021-06-11 14:30:21 +0200

    Cleanup assets

--- app/assets/javascripts/spree/frontend/spree_recently_viewed.js
+++ app/assets/javascripts/spree/frontend/spree_recently_viewed.js
@@ -1,2 +1,16 @@
 //= require spree/frontend
-//= require spree/frontend/add_recently_viewed_products
+//= require_self
+
+Spree.fetch_recently_viewed_products = (productId = "") => {
+  $.ajax({
+    url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
+    success: (data) => $("#recently_viewed_container").html(data)
+  })
+};
+
+Spree.fetch_tags_related_products = (productId = "") => {
+  $.ajax({
+    url: Spree.pathFor("tags_related_products?product_id=" + productId),
+    success: (data) => $("#tags_related_container").html(data)
+  })
+};
